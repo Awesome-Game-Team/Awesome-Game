4b2e4402715e3f8d677a9bff0b74dd7c78796601
metalx1000 <git@filmsbykris.com>
2016-05-17 19:48:16 -0400

    OSC Buttons seem to be working

--- js/osc.js
+++ js/osc.js
@@ -14,7 +14,8 @@ function createOSC(){
   var  leftBTN = game.add.button(32, game.height - 64, 'left', function(){leftOSC = true;}, this, 0, 1, 0, 1);  
   leftBTN.events.onInputOver.add(function(){leftOSC = true;},this);
   leftBTN.events.onInputOut.add(function(){leftOSC = false;},this);
-  //leftBTN.events.onInputUp.add(function(){leftOSC = false;},this);
+  leftBTN.events.onInputDown.add(function(){leftOSC = true;},this);
+  leftBTN.events.onInputUp.add(function(){leftOSC = false;},this);
   leftBTN.fixedToCamera = true;
   leftBTN.scale.setTo(.5,.5);
   leftBTN.alpha = .5;
@@ -23,7 +24,8 @@ function createOSC(){
   var rightBTN = game.add.button(128, game.height - 64, 'right', function(){rightOSC = true;}, this, 0, 1, 0, 1);
   rightBTN.events.onInputOver.add(function(){rightOSC = true;},this);
   rightBTN.events.onInputOut.add(function(){rightOSC = false;},this);
-  //rightBTN.events.onInputUp.add(function(){rightOSC = false;},this);
+  rightBTN.events.onInputDown.add(function(){rightOSC = true;},this);
+  rightBTN.events.onInputUp.add(function(){rightOSC = false;},this);
   rightBTN.fixedToCamera = true;
   rightBTN.scale.setTo(.5,.5);
   rightBTN.alpha = .5;
@@ -31,6 +33,7 @@ function createOSC(){
   //jump button
   var jumpBTN = game.add.button(game.width - 96, game.height - 64, 'trigger', playerJump, this, 0, 1, 0, 1);
   jumpBTN.events.onInputOver.add(playerJump,this);
+  jumpBTN.events.onInputDown.add(playerJump,this);
   jumpBTN.fixedToCamera = true;
   jumpBTN.scale.setTo(.5,.5);  
   jumpBTN.alpha = .5;
