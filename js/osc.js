c96946641d5cc243baf8ad1eb11673d8b7c206a6
metalx1000 <git@filmsbykris.com>
2016-05-17 19:19:44 -0400

    adjusted alpha of OSC buttons

--- js/osc.js
+++ js/osc.js
@@ -12,25 +12,28 @@ function preloadOSC(){
 function createOSC(){
   //left button
   var  leftBTN = game.add.button(32, game.height - 64, 'left', function(){leftOSC = true;}, this, 0, 1, 0, 1);  
-  //leftBTN.events.onInputOver.add(function(){leftOSC = true;},this);
+  leftBTN.events.onInputOver.add(function(){leftOSC = true;},this);
   leftBTN.events.onInputOut.add(function(){leftOSC = false;},this);
   //leftBTN.events.onInputUp.add(function(){leftOSC = false;},this);
   leftBTN.fixedToCamera = true;
   leftBTN.scale.setTo(.5,.5);
+  leftBTN.alpha = .5;
 
   //right button
   var rightBTN = game.add.button(128, game.height - 64, 'right', function(){rightOSC = true;}, this, 0, 1, 0, 1);
-  //rightBTN.events.onInputOver.add(function(){rightOSC = true;},this);
+  rightBTN.events.onInputOver.add(function(){rightOSC = true;},this);
   rightBTN.events.onInputOut.add(function(){rightOSC = false;},this);
   //rightBTN.events.onInputUp.add(function(){rightOSC = false;},this);
   rightBTN.fixedToCamera = true;
   rightBTN.scale.setTo(.5,.5);
+  rightBTN.alpha = .5;
 
   //jump button
   var jumpBTN = game.add.button(game.width - 96, game.height - 64, 'trigger', playerJump, this, 0, 1, 0, 1);
   jumpBTN.events.onInputOver.add(playerJump,this);
   jumpBTN.fixedToCamera = true;
   jumpBTN.scale.setTo(.5,.5);  
+  jumpBTN.alpha = .5;
 }
 
 
