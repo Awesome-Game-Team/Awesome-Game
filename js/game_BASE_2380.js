ec254a4668eebc9f778adbae12833b2fe7436e3d
metalx1000 <git@filmsbykris.com>
2016-05-17 20:12:29 -0400

    onscreen buttons only if in mobile device & some cleanup

--- js/game_BASE_2380.js
+++
@@ -1,9 +0,0 @@
-var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser');
-
-game.state.add('boot', bootState);
-game.state.add('load', loadState);
-game.state.add('menu', menuState);
-game.state.add('play', playState);
-game.state.add('end', endState);
-
-game.state.start('boot');
--- js/game_REMOTE_2380.js
+++
@@ -1,9 +0,0 @@
-var game = new Phaser.Game(800, 500, Phaser.AUTO, 'phaser');
-
-game.state.add('boot', bootState);
-game.state.add('load', loadState);
-game.state.add('menu', menuState);
-game.state.add('play', playState);
-game.state.add('end', endState);
-
-game.state.start('boot');
