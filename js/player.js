092550ee1c55fb19b50f49021ac34f4fc41a23a2
metalx1000 <git@filmsbykris.com>
2016-05-28 19:31:18 -0400

    put player's speed into variable

--- js/player.js
+++ js/player.js
@@ -90,6 +90,7 @@ function playerCreate(x,y,pl){
   player.jetpack = 0;
   player.jetpackActive = false;
   player.direction = "right"; 
+  player.speed = 150;
   //keyboard inputs
   //jetpack
   player.jet = game.input.keyboard.addKey(Phaser.Keyboard.J);
@@ -109,7 +110,7 @@ function playerCreate(x,y,pl){
 }
 
 function playerLeft(){
-  player.body.velocity.x = -150;
+  player.body.velocity.x = -player.speed;
   player.direction = "left";//for weapons
   if(player.body.onFloor()){
     player.animations.play('left');
@@ -119,7 +120,7 @@ function playerLeft(){
 }
 
 function playerRight(){
-  player.body.velocity.x = 150;
+  player.body.velocity.x = player.speed;
   player.direction = "right"; //for weapons
   if(player.body.onFloor()){
     player.animations.play('right');
